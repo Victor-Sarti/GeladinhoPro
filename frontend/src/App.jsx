bd53cc533201c1c91d175f80205f53b424d2da33
Victor-Sarti <Sartivictor24@gmail.com>
2025-04-09 17:01:00 -0300

    header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from './components/Header'
 
 export default function App() {
   return (
-    <h1 className=' text-xl text-red-800'>
-      OI TUDO BEM      
-    </h1>
+   <>
+   <Header/>
+   </>
   )
 }
