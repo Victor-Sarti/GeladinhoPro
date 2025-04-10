9a51ac824763b9995621604cddd76499c4fad01c
Victor-sarti <Sartivictor24@gmail.com>
2025-04-09 23:27:02 -0300

    feat: MenuNavbar

---
+++ frontend/src/components/ResponsiveMenu.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+
+const ResponsiveMenu = ({ open }) => {
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const handleClick = (index) => {
+        setActiveIndex(index);
+    };
+
+    return (
+        <AnimatePresence mode='wait'>
+            {
+                open && (
+                    <motion.div
+                        initial={{ opacity: 0, y: -100 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: -100 }}
+                        transition={{ duration: 0.3 }}
+                        className='absolute top-20 left-0 w-full h-screen z-20'
+                    >
+                        <div className='text-xl font-semibold bg-primary text-white py-10 m-6 rounded-3xl'>
+                            <ul className='flex flex-col justify-center items-center gap-10'>
+                                {['Início', 'Sobre', 'Estoque', 'Registros', 'Relatórios'].map((item, index) => (
+                                    <li
+                                        key={index}
+                                        onClick={() => handleClick(index)}
+                                        className={`cursor-pointer ${activeIndex === index ? 'text-secondary' : 'text-white'}`} 
+                                    >
+                                        {item}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </motion.div>
+                )
+            }
+        </AnimatePresence>
+    );
+}
+
+export default ResponsiveMenu;
