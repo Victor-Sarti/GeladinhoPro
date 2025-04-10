65cff90cd35dd8ce424d87a82028316b8104bbd4
Victor-sarti <Sartivictor24@gmail.com>
2025-04-10 16:06:07 -0300

    component Button

---
+++ frontend/src/components/Button/Button.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react';
+
+const Button = ({ text, onClick, className, variant = 'primary' }) => {
+    const baseClasses = 'font-semibold rounded-md border-2 px-6 py-2 duration-200 hidden md:block';
+    const variantClasses = variant === 'primary' 
+        ? 'bg-primary text-white hover:bg-white hover:text-primary border-primary'
+        : 'bg-secondary text-secondary hover:bg-secondary hover:text-white border-secondary';
+
+    return (
+        <button 
+            className={`${baseClasses} ${variantClasses} ${className}`} 
+            onClick={onClick}
+        >
+            {text}
+        </button>
+    );
+};
+
+export default Button;
