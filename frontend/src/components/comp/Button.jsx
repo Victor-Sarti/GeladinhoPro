af6b792da8d48080ee2924082ed58a4a6e018ae6
Victor-Sarti <Sartivictor24@gmail.com>
2025-04-11 16:27:38 -0300

    fix: delete md:block

--- frontend/src/components/comp/Button.jsx
+++ frontend/src/components/comp/Button.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 const Button = ({ text, onClick, className, variant = 'primary' }) => {
-    const baseClasses = 'font-semibold rounded-md border-2 px-6 py-2 duration-200 hidden md:block';
+    const baseClasses = 'font-semibold rounded-md border-2 px-6 py-2 duration-200';
     const variantClasses = variant === 'primary' 
         ? 'bg-primary text-white hover:bg-white hover:text-primary border-primary'
         : 'bg-secondary text-secondary hover:bg-secondary hover:text-white border-secondary';
