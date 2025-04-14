c1c8a75c04df7199a78eb789a698a0bf390c9949
Victor-Sarti <Sartivictor24@gmail.com>
2025-04-14 15:55:15 -0300

    add compTitle and image

--- frontend/src/components/Estoque/Estoque.jsx
+++ frontend/src/components/Estoque/Estoque.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
-
+import Title from '../comp/Title'
+import Table3 from '../../assets/Table3.svg'
 export default function Estoque() {
     return (
 
         <section>
-            <div className='flex flex-col md:flex-row items-center justify-center p-4 md:pl-28'>
-                <div className='flex flex-col justify-center items-center gap-6 w-full md:w-2/3'>
-                    <h1 className='font-semibold text-3xl'>Estoque</h1>
-                    <p className='text-stone-600'>
-                    Liste, Adicione, Altere ou Remova
-                    </p>
-                </div>
+            <div className='flex flex-col items-center justify-center p-4'>
+               <Title title={"Estoque"} desc={"Liste, Adicione, Altere ou Remova"}/>
+            <img src={Table3} alt="" />
             </div>
 
 
