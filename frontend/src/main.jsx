e8da3aa00a1da4e653249c99205a3e5ebd340afe
Victor-sarti <Sartivictor24@gmail.com>
2025-04-21 20:37:10 -0300

    feat: main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,14 +5,26 @@ import './index.css';
 import App from './App.jsx';
 import Login from './pages/Login/login.jsx'
 import Navbar from './components/Navbar.jsx'
+import Menu from './pages/Menu/Menu.jsx'
+import CadGeladinho from './pages/Cadastrar/geladiho.jsx'
+import CadProduto from './pages/Cadastrar/produto.jsx'
+import Estoque from './pages/Estoque/Geladinho.jsx'
+import EstoqueP from './pages/Estoque/Produto.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Navbar /> 
       <Routes>
+
+        <Route path="/menu" element={<Menu />} />
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} /> 
+        <Route path="/cadastrargeladinho" element={<CadGeladinho />} /> 
+        <Route path="/cadastrarproduto" element={<CadProduto />} /> 
+        <Route path="/estoqueGeladinho" element={<Estoque />} /> 
+        <Route path="/estoqueProduto" element={<EstoqueP />} /> 
+
       </Routes>
     </Router>
   </StrictMode>,
